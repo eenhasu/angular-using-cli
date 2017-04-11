853829ebf13658817a3b1ebf680f475b83b5b02c
llighter <hadbsgk@gmail.com>
2017-04-11 20:07:07 +0900

    stock in package.json and I will figure it out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { AppComponent } from './app.component';
 import { AppRoutingModule }   from './app-routing.module';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+// Imports for loading & configuring the in-memory web api
+// import { InMemoryDataService } from './in-memory-data.service'
+// import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 @NgModule({
   declarations: [
