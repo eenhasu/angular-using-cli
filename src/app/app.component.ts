1d542b1dc152a1d2351421958011b10f8e955737
llighter <hadbsgk@gmail.com>
2017-04-11 11:14:27 +0900

    make hero master/detail page (not divided)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Heroes';
+  heroes = HEROES;
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  
 }
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Iron Man'},
+  { id: 12, name: 'Torr'},
+  { id: 13, name: 'Spyder Man'},
+  { id: 14, name: 'Bat Man'},
+  { id: 15, name: 'Aqua Man'},
+  { id: 16, name: 'Yunha'},
+  { id: 17, name: 'Kill bill'},
+  { id: 18, name: 'Cheeze'},
+  { id: 19, name: 'Zoro'},
+  { id: 20, name: 'Nami'},
+];
